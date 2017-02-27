3a4e0b861474faa0f8cbefa9ed1744b8c93c1cdd
glenn mohre <glenn.mohre@gmail.com>
2017-02-27 14:47:11 -0500

    added get and set

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
 import jinja2
+from flask import jsonify
+import json
 
 app = Flask(__name__)
-
+FILE_TEMPLATE = '%(id)s_%(topic)s'
 def render(tpl_path, context):
     path, filename = os.path.split(tpl_path)
     return jinja2.Environment(
@@ -19,16 +21,24 @@ def hello():
 #	pass
 	#Write conversation to disk, throw exception on fail
 
-#@app.route("/get_conversation")
-#def getConversation(conversation):
-#	pass
-	#get by iD whatever?
+@app.route("/get_conversation")
+def getConversation(id=1, topic="coffee"):
+	filename = FILE_TEMPLATE % dict(id=id, topic=topic)
+	with open(filename) as data_file:
+	    data = json.loads(data_file.read())
+	return jsonify(data)
+
+
 
 @app.route("/render_conversation")
 def renderConversation():
 	conversation = {'timestamp':1,'numResponses':1, 'text':"hey\nyou\nguy", 'topic':"coffee", 'id':1}
-	result = render('chat.html', conversation)
-	return result
+	filename = FILE_TEMPLATE % conversation
+	jsonData = jsonify(conversation)
+	with open(filename, 'w') as outfile:
+		json.dump(conversation, outfile)
+	return jsonData
+
 
 if __name__ == "__main__":
     app.run
