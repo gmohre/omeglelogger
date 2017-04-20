7116f57700117bfd173919459937ea76aa6d7010
gmohre <glenn.mohre@gmail.com>
2017-04-20 15:43:51 -0400

    fixed some garbo

--- __init__.py
+++ __init__.py
@@ -18,31 +18,32 @@ def render(tpl_path, context):
 
 @app.route("/")
 def home():
-	return render_template('chatToCanvas.html')
-@app_route("get_from_omegle")
-def get_from_omegle():
+    return render_template('chatToCanvas.html')
+
+#@app_route("get_from_omegle")
+#def get_from_omegle():
 
 
 @app.route("/get_conversation/<topic>/<id>")
 def getConversation(topic, id):
-	filename = FILE_TEMPLATE % dict(id=id, topic=topic)
-	with open(filename) as data_file:
-	    data = json.loads(data_file.read())
-	return jsonify(data)
+    filename = FILE_TEMPLATE % dict(id=id, topic=topic)
+    with open(filename) as data_file:
+        data = json.loads(data_file.read())
+    return jsonify(data)
 
 
 
 @app.route("/render_conversation/<topic>/<id>")
 def renderConversation(topic, id):
-	text = "heybigyoubigguy"
-	numLines = str.count(text, "\n") + 1
-	conversation = {'timestamp':1,'numLines':numLines, 'text':text, 'topic':"coffee", 'id':1}
-	filename = FILE_TEMPLATE % conversation
-	jsonData = jsonify(conversation)
-	with open(filename, 'w') as outfile:
-		json.dump(conversation, outfile)
-	return "Rendered %s to disk at %s" %(conversation['text'], filename)
+    text = "heybigyoubigguy"
+    numLines = str.count(text, "\n") + 1
+    conversation = {'timestamp':1,'numLines':numLines, 'text':text, 'topic':"coffee", 'id':1}
+    filename = FILE_TEMPLATE % conversation
+    jsonData = jsonify(conversation)
+    with open(filename, 'w') as outfile:
+            json.dump(conversation, outfile)
+    return "Rendered %s to disk at %s" %(conversation['text'], filename)
 
 
 if __name__ == "__main__":
-    app.run
+    app.run()
